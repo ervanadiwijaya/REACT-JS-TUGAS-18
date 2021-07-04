6b6cf5045bbf1dd2f6186e38f1126ddb599722dd
jauhhari <ervanadiwijaya@gmail.com>
2021-07-04 21:58:04 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Slider, Caption, Slide, SideNav, SideNavItem, Row, Col, Card, CardTitle, Icon, Button, Pagination } from "react-materialize";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <SideNav
+        id="SideNav-10"
+        options={{
+          draggable: true
+        }}
+        trigger={<Button node="button"><i className="material-icons">list</i></Button>}
+      >
+        <SideNavItem
+          user={{
+            background: 'https://placeimg.com/640/480/tech',
+            email: 'test@gmail.com',
+            image: 'https://media.suara.com/pictures/653x366/2020/04/22/25823-anya-geraldine.jpg',
+            name: 'Satria Darmawan'
+          }}
+          userView
+        />
+        <SideNavItem
+          href="#!icon"
+          icon={<Icon>account_circle</Icon>}
         >
-          Learn React
-        </a>
-      </header>
+          Profil saya
+        </SideNavItem>
+        <SideNavItem
+
+          href="#!second"
+          icon={<Icon>help</Icon>}
+        >
+          Hubungi kami
+        </SideNavItem>
+        <SideNavItem divider />
+        <SideNavItem subheader>
+          Product
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Kesehatan
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Komputer & Aksesoris
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Gaming
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Kamera
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Olahraga
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Fashion Pria
+        </SideNavItem>
+        <SideNavItem
+          href="#!third"
+          waves
+        >
+          Fashion Wanita
+        </SideNavItem>
+      </SideNav>
+
+      <Slider
+        fullscreen={false}
+        options={{
+          duration: 500,
+          height: 400,
+          indicators: true,
+          interval: 6000
+        }}
+      >
+        <Slide image={<img alt="" src=" https://s-ec.bstatic.com/images/hotel/max1024x768/106/106114417.jpg" />}>
+          <Caption placement="center">
+            <h3 className="black-text">
+              Pantai
+            </h3>
+            <h5 className="light black-text text-lighten-3">
+              Dapatkan Kesempatan Jalan - Jalan Ke Pantai Dengan Membeli Sebuah Product
+            </h5>
+          </Caption>
+        </Slide>
+        <Slide image={<img alt="" src="https://assets.kompasiana.com/items/album/2021/06/14/pexels-photo-169647-60c76664d541df60d07bb892.jpeg?t=o&v=740&x=416" />}>
+          <Caption placement="center">
+            <h3>
+              Pemandangan Kota
+            </h3>
+            <h5 className="light grey-text text-lighten-3">
+              Nikmati Keindahan Kota Pada Malam Hari
+            </h5>
+          </Caption>
+        </Slide>
+        <Slide image={<img alt="" src="https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?wid=2880&hei=1280&fmt=jpeg&crop=9,336,2699,1200&anchor=1358,936&qlt=75,0&fit=wrap&op_sharpen=0&resMode=sharp2&op_usm=0,0,0,0&iccEmbed=0&printRes=72" />}>
+          <Caption placement="right">
+            <h3>
+              Paris
+            </h3>
+            <h5 className="light grey-text text-lighten-3">
+              Nikmati keindahan kota dari menara Eifel
+            </h5>
+          </Caption>
+        </Slide>
+      </Slider>
+      <h4 className="center">Hot List</h4>
+      <Row>
+        <Col
+          m={3}
+          s={12}
+        >
+          <Card className="small"
+            header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/8/15/25749302/25749302_274a89a6-ea41-4a3a-ac6b-ce2881016369.jpg">Audio</CardTitle>}
+          >
+            Audio Technica - mulai dari <span className="red white-text">Rp. 910.0000,-</span>
+          </Card>
+        </Col>
+        <Col
+          m={3}
+          s={12}
+        >
+          <Card className="small"
+            header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2019/1/16/25287362/25287362_a79f0aa6-cf1f-484f-b1a8-83993245cd6f.jpg">Handphone</CardTitle>}
+          >
+            Handphone Samsung - mulai dari <span className="red white-text">Rp. 200.0000,-</span>
+          </Card>
+        </Col>
+        <Col
+          m={3}
+          s={12}
+        >
+          <Card className="small"
+            header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/6/26/28201197/28201197_856195f2-b0ba-4711-8200-fd95beaf1df5.jpg">Sequishy</CardTitle>}
+          >
+            Sequishy - mulai dari <span className="red white-text">Rp. 5.0000,-</span>
+          </Card>
+        </Col>
+        <Col
+          m={3}
+          s={12}
+        >
+          <Card className="small"
+            header={<CardTitle image="https://ecs7.tokopedia.net/img/cache/300/attachment/2018/4/23/28201197/28201197_0383072a-1766-453c-951a-8b53a719cff0.jpg">Koleksi The Avenger</CardTitle>}
+          >
+            Koleksi The Avenger - mulai dari <span className="red white-text">Rp. 10.0000,-</span>
+          </Card>
+        </Col>
+      </Row>
+      <Pagination
+        activePage={2}
+        items={10}
+        leftBtn={<Icon>chevron_left</Icon>}
+        maxButtons={8}
+        rightBtn={<Icon>chevron_right</Icon>}
+      />
     </div>
   );
 }
